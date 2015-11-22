8dd02f763722cdb53ca55ae48e89af0ab68789af
ershov-konst <ershov.konst@gmail.com>
2015-11-22 22:59:45 +0300

    - fixed markup with excess closing tag - test

--- test/invalidHTML.js
+++ test/invalidHTML.js
@@ -41,4 +41,45 @@ describe('invalid html', function () {
     assert.equal(compareResult.getResult(), true, reporter.report(compareResult));
 
   });
+
+  it ('excess closing tag', function(){
+    var
+      invalidHTML =
+        '<div id="root">\n' +
+        '  <span>\n' +
+        '    <div class="broken">\n' +
+        '      <div class="inner">1</div>\n' +
+        '    </div>\n' +
+        '    </div>\n' +
+        '  </span>\n' +
+        '  <span>\n' +
+        '    <div class="broken">\n' +
+        '      <div class="inner">1</div>\n' +
+        '    </div>\n' +
+        '    </div>\n' +
+        '  </span>\n' +
+        '</div>\n'
+      ,
+      validHTML =
+        '<div id="root">\n' +
+        '  <span>\n' +
+        '    <div class="broken">\n' +
+        '      <div class="inner">1</div>\n' +
+        '    </div>\n' +
+        '  </span>\n' +
+        '  <span>\n' +
+        '    <div class="broken">\n' +
+        '      <div class="inner">1</div>\n' +
+        '    </div>\n' +
+        '  </span>\n' +
+        '</div>';
+
+    var
+      dom = parser.parseFromString(invalidHTML),
+      ctn = dom.getElementById('root');
+
+    var compareResult = compareStrings(ctn.outerHTML, validHTML);
+
+    assert.equal(compareResult.getResult(), true, reporter.report(compareResult));
+  });
 });

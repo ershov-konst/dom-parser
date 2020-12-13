9a90353b3547fbf6c60d30ca769eebe9f0cb81a0
sahrun <sharunkk@eyerov.com>
2020-12-13 11:44:24 +0530

    query selector for all elemets without id and class filter

--- example/example.js
+++ example/example.js
@@ -1,4 +1,4 @@
 var DOMPraser = require('../index');
 const DOM = new DOMPraser
-let data =  "<div id='h2' class='ha he' data='da'>dada</div><div id='h1' class='ha he'>ha he</div>"
-console.log(DOM.parseFromString(data).getElementById('h2').classList)
+let data =  "<div id='h2' class='ha he' data='da'>dada</div><div id='h1' class='ha he'>ha he</div><li>ha hau</li>"
+console.log(DOM.parseFromString(data).querySelectorAll('div,li'))
